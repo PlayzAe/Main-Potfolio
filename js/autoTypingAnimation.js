823d33688f3334c9593381368345370681b0de80
George <icannotp@gmail.com>
2024-11-26 17:19:33 +0100

    main

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>I’m George, a passionate backend developer with expertise in C# and API design, specializing in MVC and REST API development. With over 3 years of experience in web development, I focus on building scalable and reliable APIs that simplify user workflows. Additionally, I have 4 years of experience as a motion graphics designer, skilled in creating stunning visuals with After Effects."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
